d48c4d648fb75bb322241fd99d77333dab617a46
Julian Guigon <julian.guigon.jg@gmail.com>
2017-12-12 13:54:39 +0100

    tp

---
+++ Config/config.php
@@ -0,0 +1,8 @@
+<?php
+    $phraseComplique='mysql:host=localhost;dbname=test;charset=utf8';
+    $login='root';
+    $mdp='';
+
+    require_once("Controller/Controller.php");
+    require_once("Connection/Connection.php");
+?>
---
+++ Vues/PageConnection.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PageConnection</title>
+	<meta charset="utf-8"/>
+</head>
+<body>
+	<pre>
+Bonjour
+
+<form method="POST">
+nom d'utilisateur	<input type="text" name="login"/>
+mot de passe	 	<input type="password" name="password"/>
+<input type="submit" name="var" value="Envoyer"/>
+<input type="hidden" name="action" value="valider"/>
+</form>
+	</pre>
+</body>
+</html>
